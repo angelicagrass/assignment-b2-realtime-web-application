0afddb8131a881b056f0ca8b062c92dfc60f4122
Angelica Grass <ag223vg@student.lnu.se>
2021-02-10 15:43:13 +0100

    hook-controller

---
+++ src/controllers/hook-controller.js
@@ -0,0 +1,70 @@
+/**
+ * Module for the TasksController.
+ *
+ * @author Johan Leitet
+ * @version 1.0.0
+ */
+
+/**
+ * Encapsulates a controller.
+ */
+export class HookController {
+  /**
+   * Recieves a Webhook, validates it and sends it to Tasks Create Controller.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  index (req, res, next) {
+    console.log('HOOK INDEX')
+    console.log(req.body)
+    req.body = {
+      description: req.body.object_attributes.description,
+      title: req.body.object_attributes.title,
+      id: req.body.object_attributes.id,
+      avatar: req.body.user.avatar_url,
+      state: req.body.object_attributes.state
+    }
+    next()
+  }
+
+  async updateIssue (req, res, next) {
+    console.log('UPDATEISSUE')
+    try {
+      res.io.emit('issue', {
+        title: req.body.title,
+        description: req.body.description,
+        avatar: req.body.avatar,
+        id: req.body.id
+      })
+
+      if (req.headers['x-gitlab-event']) {
+        res.status(200).send('Hook accepted')
+        return
+      }
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+   * Authorizes the webhook.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  authorize (req, res, next) {
+    console.log('authorize')
+    console.log(req.headers)
+    // Validate the Gitlab Secret Token to be sure that the hook is from the correct sender.
+    // This need to be in a database if we have multiple users.
+    if (req.headers['x-gitlab-token'] !== process.env.HOOK_SECRET) {
+      res.status(403).send('Incorrect Secret')
+      return
+    }
+
+    next()
+  }
+}
